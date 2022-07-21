e0b2d80a7022c8085f55be7b0003b8fc88d27690
imantha-das <imantha@yahoo.com>
2022-07-21 15:16:58 +0800

    some minor updates

--- main.jl
+++ main.jl
@@ -1,35 +1,46 @@
-using Agents: AbstractAgent, AgentBasedModel, Schedulers, add_agent!, ABM
+using Pkg
+Pkg.activate("envs/abm")
+using Agents: AbstractAgent, AgentBasedModel, Schedulers, add_agent!, ABM, nagents
 include("CrttAgents.jl")
 
 function initialise(num_cas,num_resc)
     model = ABM(
-        Cas,
+        Union{Cas,Resc},
         scheduler = Schedulers.fastest,
         properties = Dict(
             :ticks => 0, 
-            :dist_per_step => 50,
-            :dist_from_vx => 2000,
-            :dist_from_sm => 2500,
-            :dist_from_hosp => 10000
+            :dist_per_step => 50.,
+            :dist_from_vx => 2000.,
+            :dist_from_sm => 2500.,
+            :dist_from_hosp => 10000.
         )
     )
 
+    # Add Casualties
     for i = 1:num_cas
         cas = Cas(
             i,
-            rand([1,2,3]);
-            dist_from_vx = model.dist_from_vx,
-            dist_from_sm = model.dist_from_sm,
-            dist_from_hosp = model.dist_from_hosp,
-            rescued_by = 999, #just an initialised value
-            at_pma = 999 # just an initialised value
+            rand([1,2,3]),
+            model.dist_from_vx,
+            model.dist_from_sm,
+            model.dist_from_hosp
         )
-        add_agent!(model, Cas)
+        add_agent!(cas, model)
+    end
+
+    # Add Rescuers 
+    for i = nagents(model):nagents(model) + num_resc
+        resc = Resc(
+            i
+        )
+        add_agent!(resc,model)
     end
 
     return model
 end
 
-model = initialise(100, 5)
+model = initialise(20,5)
+
+
 
 
