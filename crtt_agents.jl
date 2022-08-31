9873ed50b320bdb2784d16eee86d7bc28315b60c
imantha-das <imantha@yahoo.com>
2022-08-31 10:50:17 +0800

    included structs for PMA and Hosp

--- crtt_agents.jl
+++ crtt_agents.jl
@@ -27,21 +27,44 @@ module CrttAgents
 
     mutable struct Resc <: AbstractAgent
         const id::Int
+        action::DataType
+        rescued::Vector{Int}
         dist_to_iz::Float64
         dist_to_vx::Float64
         dist_to_sm::Float64
         dist_to_hosp::Float64
-        rescued::Vector{Int}
         at_iz::Bool
-        on_way_to_iz:Bool
+        on_way_to_iz::Bool
         at_pma::Bool
         on_way_to_pma::Bool
         at_hosp::Bool
         on_way_to_hosp::Bool
 
-        function Resc(id;dist_to_iz=999,dist_to_vx=999,dist_to_sm=999,dist_to_hosp=999,rescued = Int[],at_iz = false, on_way_to_iz = false, at_pma = true, on_way_to_pma = false, at_hosp = false, on_way_to_hosp=false)
-            new(id, dist_to_iz,dist_to_vx,dist_to_sm,dist_to_hosp,rescued,at_iz,on_way_to_iz,at_pma,on_way_to_pma,at_hosp,on_way_to_hosp)
+        function Resc(id;action=IZ,rescued = Int[],dist_to_iz=999,dist_to_vx=999,dist_to_sm=999,dist_to_hosp=999,at_iz = false, on_way_to_iz = false, at_pma = true, on_way_to_pma = false, at_hosp = false, on_way_to_hosp=false)
+            new(id,action,rescued,dist_to_iz,dist_to_vx,dist_to_sm,dist_to_hosp,at_iz,on_way_to_iz,at_pma,on_way_to_pma,at_hosp,on_way_to_hosp)
         end
     end
 
+    mutable struct PMA <: AbstractAgent
+        const id::Int
+        name::String
+        pre_stabilize_queue::Vector{Int} 
+        in_stabilization::Vector{Tuple{Int,Int}} #[(Cas_id, ticks), ...]
+        post_stabilize_queue::Vector{Int}
+
+        function PMA(id;name="VX", pre_stabilize_queue=[],in_stabilization=[],post_stabilize_queue=[])
+            new(id,name,pre_stabilize_queue,in_stabilization,post_stabilize_queue)
+        end
+    end
+
+    mutable struct Hosp <: AbstractAgent
+        const id::Int 
+        pre_treatment_queue::Vector{Int}
+        burn_beds::Vector{Int}
+        non_burn_beds::Vector{Int}
+
+        function Hosp(id;pre_treatment_queue=[],burn_beds=[],non_burn_beds=[])
+            new(id,pre_treatment_queue,burn_beds,non_burn_beds)
+        end
+    end
 end
